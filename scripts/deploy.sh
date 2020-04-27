54ba5d9dbb2cfa241f67d06789e02679ae122a73
Luan Nico <luanpotter27@gmail.com>
2020-04-26 22:45:16 -0400

    Add version to hello route

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -xe
 
+version=`git log --pretty=format:'%h' -n 1`
+
 rm -rf dist
 
 cp -r server dist
 rm dist/public
 cp -r client/dist dist/public
+
+perl -i -pe "s/const VERSION = 'dev'/const VERSIOn = '$version'/" dist/src/app.js
 
 git subtree push --prefix dist heroku master
