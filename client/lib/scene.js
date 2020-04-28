eaed0e5579cbab4c4539dc688b14f1f2f4f514ed
Luan Nico <luanpotter27@gmail.com>
2020-04-27 22:01:33 -0400

    Allow you to talk locally (no network yet)

--- client/lib/scene.js
+++ client/lib/scene.js
@@ -26,6 +26,7 @@ export const SceneFactory = room => class Scene extends Phaser.Scene {
 
 	create() {
 		this.messageDiv = document.querySelector('#write-message');
+		this.messageInput = this.messageDiv.querySelector('input');
 		this.isTyping = false;
 
 		const map = this.make.tilemap({
@@ -39,21 +40,35 @@ export const SceneFactory = room => class Scene extends Phaser.Scene {
 		this.player = new Player(this, 7, 8);
 		this.cameras.main.startFollow(this.player.sprite, true);
 
-		this.keys = this.input.keyboard.addKeys('W,A,S,D');
+		this.keys = this.input.keyboard.addKeys('W,A,S,D', false);
 
-		this.input.keyboard.on('keydown_T', () => {
-			this.isTyping = true;
-			this.messageDiv.style.display = 'block';
-			this.messageDiv.querySelector('input').focus();
-			return true;
+		this.input.keyboard.on('keydown_T', e => {
+			if (this.isTyping) {
+				return;
+			}
+			this.setTyping(true);
+			this.messageInput.focus();
+			this.messageInput.value = '';
+			e.stopPropagation();
+			e.preventDefault();
 		});
 		this.input.keyboard.on('keydown_ESC', () => {
-			this.isTyping = false;
-			this.messageDiv.style.display = 'none';
-			return true;
+			this.setTyping(false);
+		});
+		this.input.keyboard.on('keydown_ENTER', () => {
+			this.setTyping(false);
+			const message = this.messageInput.value;
+			this.messageInput.value = '';
+			// TODO broadcast message
+			this.player.speak(message);
 		});
 
 		this.gameStateManager = new GameStateManager(room, this);
+	}
+
+	setTyping(value) {
+		this.isTyping = value;
+		this.messageDiv.style.display = value ? 'block' : 'none';
 	}
 
 	update(_, dt) {
