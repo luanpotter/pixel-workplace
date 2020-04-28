22bbf402449a4cf83577156a87961dcd8c75ee0c
Luan Nico <luanpotter27@gmail.com>
2020-04-27 20:55:54 -0400

    Fix ssl on heroky, hopefully

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const forceSSL = require('express-force-ssl')
 const colyseus = require('colyseus')
 const { createServer } = require('http')
 const Workspace = require('./rooms/workspace')
 
+const ENV = process.env.NODE_ENV || 'development'
 const VERSION = process.env.HEROKU_SLUG_COMMIT || 'dev'
+
+const forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''))
+  }
+  return next()
+}
 
 const app = express()
 app.use(express.json())
-app.use(forceSSL)
+if (ENV !== 'development') {
+  app.use(forceSSL)
+}
 
 app.get('/hello', (req, res) => res.send(`Welcome to Pixel Workspace! Version: ${VERSION}`))
 
